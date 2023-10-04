8584c49b34edd56c4a8887956a9f41efddc66df3
mohammad subkhan <subkhanpinter22@gmail.com>
2023-10-04 09:25:45 +0700

    feat: add provider and aliases on app config

--- config/app.php
+++ config/app.php
@@ -166,6 +166,8 @@ return [
          * Package Service Providers...
          */
 
+         SimpleSoftwareIO\QrCode\QrCodeServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
@@ -174,6 +176,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        
 
     ],
 
@@ -229,6 +232,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'QrCode' => SimpleSoftwareIO\QrCode\Facades\QrCode::class,
 
     ],
 
